1afda29e89586b3ad1af9d91b702782e4b91423b
David Martin <david@chaosiq.io>
2020-08-14 11:24:40 +0200

    Add shell backend for running local 'chaos' command Handle no TLS verification as 'chaos' option for run/verify commands Add org & team for job (same as experiment/verification) to be used in default CTK settings Change access token type from bytes to string (caused 403 with "b'...'" used as full token by ctk)
    Signed-off-by: David Martin <david@chaosiq.io>

--- chaosiqagent/backend/shell.py
+++ chaosiqagent/backend/shell.py
@@ -1,5 +1,12 @@
+import os
+import shlex
+import subprocess
+import tempfile
+
 from ..types import Config, Job
 from .base import BaseBackend
+from ..ctk import get_chaostoolkit_settings
+from ..log import logger
 
 __all__ = ["ShellBackend"]
 
@@ -7,16 +14,41 @@ __all__ = ["ShellBackend"]
 class ShellBackend(BaseBackend):
     def __init__(self, config: Config) -> None:
         BaseBackend.__init__(self, config)
+        self.bin = self.config.chaos_binary
 
     async def setup(self) -> None:
-        raise NotImplementedError()
+        # ensure the `chaos` binary path is defined & exists
+        if not self.bin:
+            logger.critical("'chaos' binary path must be set in config!")
+        if not os.path.exists(self.bin):
+            logger.critical("'chaos' binary path cannot be found, "
+                            "please check your config!")
+        logger.info(f"Backend '{self.name}' configured with "
+                    f"Chaos Toolkit binary: {self.bin}")
 
     async def cleanup(self) -> None:
-        raise NotImplementedError()
+        self.bin = None
 
     async def process_job(self, job: Job) -> None:
         """
         Uses the Chaos Toolkit's `chaos` command on the local shell
         to run the experiments.
         """
-        raise NotImplementedError()
+        settings = get_chaostoolkit_settings(
+            self.config, job.access_token,
+            org_id=job.org_id, team_id=job.team_id)
+
+        with tempfile.NamedTemporaryFile(mode="w") as f:
+            f.write(settings)
+            f.seek(0)
+
+            # full_cmd = f"{self.bin} --settings {f.name} info settings"
+            cmd = "verify" if job.target_type == "verification" else "run"
+            full_cmd = f"{self.bin} --settings {f.name} {cmd} {job.target_url}"
+
+            # handle no verify TLS option
+            if not self.config.verify_tls:
+                full_cmd = f"{full_cmd} --no-verify-tls"
+
+            p = subprocess.run(shlex.split(full_cmd))
+            p.check_returncode()
--- chaosiqagent/types.py
+++ chaosiqagent/types.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import Literal, Optional, Dict, Any
 
 from pydantic import BaseModel, BaseSettings, Field, UUID4, AnyUrl
+from pydantic.fields import Undefined
 
 __all__ = ["Config", "Job", "Backend"]
 
@@ -17,16 +18,19 @@ class Config(BaseSettings):
     agent_backend: Backend = Field(
         "null", env='AGENT_BACKEND')
     verify_tls: bool = Field(False, env='VERIFY_TLS')
+    chaos_binary: str = Field(Undefined, env='CHAOS_BINARY')
 
 
 class Job(BaseModel):
     id: UUID4
     agent_id: UUID4
+    org_id: UUID4
+    team_id: Optional[UUID4]
 
     target_id: UUID4
     target_type: Literal["experiment", "verification"]
     target_url: AnyUrl
-    access_token: Optional[bytes]
+    access_token: Optional[str]
 
     payload: Optional[Dict[str, Any]]
     run_at: Optional[datetime]
